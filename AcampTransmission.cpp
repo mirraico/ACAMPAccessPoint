2fabddefff0761f6b99e584730360271d68eb5a4
mirraico <feixuan2013@gmail.com>
2016-04-13 16:50:38 +0800

    MOD: network byte order in encapsulate

--- AcampTransmission.cpp
+++ AcampTransmission.cpp
@@ -1,19 +1,18 @@
 #include "AcampTransmission.h"
 
 sockaddr_in udp_addrin;
-int udp_init(char* udp_addr, int udp_port)
+int udp_fd;
+
+void udp_init(char* udp_addr, int udp_port)
 {
-    int udp_sock;
     udp_addrin.sin_family = AF_INET;
     udp_addrin.sin_addr.s_addr = inet_addr(udp_addr);
     udp_addrin.sin_port = htons(udp_port);
-    udp_sock = socket(AF_INET,SOCK_DGRAM,0);
-    return udp_sock;
+    udp_fd = socket(AF_INET,SOCK_DGRAM,0);
 }
 
-
-int udp_send(int fd, u8* buf, int len)
+int udp_send(u8* buf, int len)
 {
-    return sendto(fd, buf, len, 0,
+    return sendto(udp_fd, buf, len, 0,
                   (sockaddr*)&udp_addrin,sizeof(udp_addrin));
 }
